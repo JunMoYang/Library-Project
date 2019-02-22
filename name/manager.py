014f3b3746f8ee177894cafa833cdcf3bf448d16
kioryu <1218moonlight@gmail.com>
2019-02-23 00:52:36 +0900

    2개의 이름에 관련된 기능을 포함하는 "namesManager" 추가.

--- name/manager.py
+++ name/manager.py
@@ -25,7 +25,31 @@ class nameManager:
         return True
 
     def checkLen(self):
-        if len(self.__name) == 3 :
+        if len(self.__name) == 3:
             return True
         else:
             return False
+
+
+# "namesManager"은 '2개의 이름'에 관련된 기능이 포함됩니다.
+class namesManager:
+    def __init__(self, name1, name2):
+        self.__name = {}
+        self.__name1 = name1
+        self.__name2 = name2
+
+    # 'name1'과 'name2'의 이름을 번갈아 가면서 'name'에 기입합니다.
+    # 'name1[0]', 'name2[0]', 'name1[1]', 'name2[1]' ...
+    def mixNames(self):
+        for i in range(3):
+            if i != 0:
+                self.__name[i + i] = self.__name1[i]
+            else:
+                self.__name[i] = self.__name1[i]
+        for i in range(3):
+            if i == 0:
+                self.__name[i + 1] = self.__name2[i]
+            else:
+                self.__name[i + i + 1] = self.__name2[i]
+
+        return self.__name
--- name/test.py
+++ name/test.py
@@ -1,10 +1,13 @@
 from name import manager
 
 hong = manager.nameManager("홍길동")
-if hong.isString():
-    if hong.checkLen():
-        if hong.isKorean():
-            print(hong.getName())
+kim = manager.nameManager("김두령")
+
+if hong.isString() and kim.isString():
+    if hong.checkLen() and kim.checkLen():
+        if hong.isKorean() and kim.isKorean():
+            names = manager.namesManager(hong.getName(), kim.getName())
+            print(names.mixNames())
         else:
             print("Error Language")
     else:
