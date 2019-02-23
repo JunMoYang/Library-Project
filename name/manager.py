8ab0c0d8d7429c76418233145d41b6714b181552
kioryu <1218moonlight@gmail.com>
2019-02-23 23:56:30 +0900

    nameManager and namesManager의 병합및 사용 단순화.

--- name/manager.py
+++ name/manager.py
@@ -2,36 +2,57 @@ import operator
 import unicodedata
 
 
-# "nameManager"는 'name'의 '유효성 검사'및 '필요한 기능'들이 포함됩니다.
-class nameManager:
-    def __init__(self, name):
-        self.__name = name
+# "namesManager"는 'name'의 '유효성 검사'및 '필요한 기능'들이 포함됩니다.
+class namesManager:
+    def __init__(self, name1, name2):
+        self.__name1 = name1
+        self.__name2 = name2
 
-    def getName(self):
-        return self.__name
+    # 'name1'과 'name2'의 이름을 번갈아 가면서 'name'에 기입합니다.
+    # 'name1[0]', 'name2[0]', 'name1[1]', 'name2[1]' ...
+    def mixNames(self):
+        name = {}
+        if self.__verification():
+            for i in range(3):
+                if i != 0:
+                    name[i + i] = self.__name1[i]
+                else:
+                    name[i] = self.__name1[i]
+            for i in range(3):
+                if i == 0:
+                    name[i + 1] = self.__name2[i]
+                else:
+                    name[i + i + 1] = self.__name2[i]
+
+            sortedNames = sorted(name.items(), key=operator.itemgetter(0))
+            result = list()
+            for i in range(len(sortedNames)):
+                result.append(sortedNames[i][1])
+
+            return result
 
     # 'type', '길이', '언어'를 체크.
-    def verification(self):
-        if not self.__isString():
+    def __verification(self):
+        if not self.__isString(self.__name1) and not self.__isString(self.__name2):
             print("Error Type")
             return False
-        elif not self.__checkLen():
+        elif not self.__checkLen(self.__name1) and not self.__checkLen(self.__name2):
             print("Error Length")
             return False
-        elif not self.__isKorean():
+        elif not self.__isKorean(self.__name1) and not self.__isKorean(self.__name2):
             print("Error Language")
             return False
         else:
             return True
 
-    def __isString(self):
-        if type(self.__name) == str:
+    def __isString(self, name):
+        if type(name) == str:
             return True
         else:
             return False
 
-    def __isKorean(self):
-        for v in self.__name:
+    def __isKorean(self, name):
+        for v in name:
             if unicodedata.name(v).find("HANGUL") != -1:
                 continue
             else:
@@ -39,37 +60,8 @@ class nameManager:
 
         return True
 
-    def __checkLen(self):
-        if len(self.__name) == 3:
+    def __checkLen(self, name):
+        if len(name) == 3:
             return True
         else:
             return False
-
-
-# "namesManager"은 '2개의 이름'에 관련된 기능이 포함됩니다.
-class namesManager:
-    def __init__(self, name1, name2):
-        self.__name = {}
-        self.__name1 = name1
-        self.__name2 = name2
-
-    # 'name1'과 'name2'의 이름을 번갈아 가면서 'name'에 기입합니다.
-    # 'name1[0]', 'name2[0]', 'name1[1]', 'name2[1]' ...
-    def mixNames(self):
-        for i in range(3):
-            if i != 0:
-                self.__name[i + i] = self.__name1[i]
-            else:
-                self.__name[i] = self.__name1[i]
-        for i in range(3):
-            if i == 0:
-                self.__name[i + 1] = self.__name2[i]
-            else:
-                self.__name[i + i + 1] = self.__name2[i]
-
-        sortedNames = sorted(self.__name.items(), key=operator.itemgetter(0))
-        result = list()
-        for i in range(len(sortedNames)):
-            result.append(sortedNames[i][1])
-
-        return result
--- name/test.py
+++ name/test.py
@@ -1,8 +1,4 @@
 from name import manager
 
-hong = manager.nameManager("홍길동")
-kim = manager.nameManager("김두령")
-
-if hong.verification() and kim.verification():
-    names = manager.namesManager(hong.getName(), kim.getName())
-    print(names.mixNames())
+nManager = manager.namesManager("홍길동", "김기덕")
+print(nManager.mixNames())
